7850ade91245350831dea3f3474ae95ed4d3391e
Suyeon Kang <tndusrkd92@gmail.com>
2022-08-09 21:51:58 +0900

    feat: Update the way to manage env file

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,12 +1,10 @@
 import dotenv from 'dotenv';
 
-dotenv.config({ path: 'src/config/.env' });
+dotenv.config();
 
 export default {
   PORT: process.env.PORT,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME
+  DB_NAME: process.env.DB_NAME,
 }
-
-
